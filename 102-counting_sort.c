b1bb1f0d3a46855b340c3205da30f2b77ffff765
Birhan Abuhay Jemere <124788842+TgGeda@users.noreply.github.com>
2023-04-24 23:21:58 +0300

    Some correction in algorithms

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -6,55 +6,50 @@
  *@size: bit size of each element
  *Return: pointer to memory assignement
  */
-void *_calloc(unsigned int nmemb, unsigned int size)
-{
-	unsigned int i = 0;
-	char *p;
+#include <stdio.h>
+#include <stdlib.h>
 
-	if (nmemb == 0 || size == 0)
-		return ('\0');
-	p = malloc(nmemb * size);
-	if (p == '\0')
-		return ('\0');
-	for (i = 0; i < (nmemb * size); i++)
-		p[i] = '\0';
-	return (p);
+void counting_sort(int *array, size_t size) {
+    int max = 0;
+    for (size_t i = 0; i < size; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+    int *count = (int *) malloc((max + 1) * sizeof(int));
+    if (count == NULL) {
+        printf("Error: Failed to allocate memory for count array\n");
+        return;
+    }
+    for (int i = 0; i <= max; i++) {
+        count[i] = 0;
+    }
+    for (size_t i = 0; i < size; i++) {
+        count[array[i]]++;
+    }
+    printf("Count array: ");
+    for (int i = 0; i <= max; i++) {
+        printf("%d ", count[i]);
+    }
+    printf("\n");
+    int index = 0;
+    for (int i = 0; i <= max; i++) {
+        for (int j = 0; j < count[i]; j++) {
+            array[index] = i;
+            index++;
+        }
+    }
+    free(count);
 }
-/**
- * counting_sort - this is a counting sort method implementation
- * @array: array to sort
- * @size: array size
- */
-void counting_sort(int *array, size_t size)
-{
-	int index, maximun = 0, *counter = '\0', *tmp = '\0';
-	size_t i;
 
-	if (array == '\0' || size < 2)
-		return;
-	/* find maximun number */
-	for (i = 0; i < size; i++)
-		if (array[i] > maximun)
-			maximun = array[i];
-	counter = _calloc(maximun + 1, sizeof(int));
-	tmp = _calloc(size + 1, sizeof(int));
-	/* count the array elements */
-	for (i = 0; i < size; i++)
-		counter[array[i]]++;
-	/* get the accumulative values */
-	for (index = 1; index <= maximun; index++)
-		counter[index] += counter[index - 1];
-	print_array(counter, maximun + 1);
-	/* get the new array sorted */
-	for (i = 0; i < size; ++i)
-	{
-		tmp[counter[array[i]] - 1] = array[i];
-		counter[array[i]]--;
-	}
-	/* replace old array to new array sorted */
-	for (i = 0; i < size; i++)
-		array[i] = tmp[i];
-	free(tmp);
-	free(counter);
-
+int main() {
+    int arr[] = {5, 2, 4, 6, 1, 3};
+    size_t size = sizeof(arr) / sizeof(arr[0]);
+    counting_sort(arr, size);
+    printf("Sorted array: ");
+    for (size_t i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
 }
